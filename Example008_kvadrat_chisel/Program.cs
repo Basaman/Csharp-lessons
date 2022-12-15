c77ad1c601e309b6a81a298d5cadeb388c500aef
ilya <ilia.arkhangelsky@gmail.com>
2022-12-15 20:53:41 +0300

    ex008 kvadrat chisel

---
+++ Example008_kvadrat_chisel/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите целое число: ");
+string userInput = Console.ReadLine() ?? "";
+
+int number = int.Parse(userInput);
+int result = number * number;
+
+Console.WriteLine($"{number} -> {result}");
