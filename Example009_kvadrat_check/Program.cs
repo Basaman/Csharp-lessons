fdc5674f047f8b09c4731577a131356e68ee11b0
ilya <ilia.arkhangelsky@gmail.com>
2022-12-15 21:45:47 +0300

    Ex009 proverka kvadrata

---
+++ Example009_kvadrat_check/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите целое число: ");
+string userInput_1 = Console.ReadLine();
+Console.Write("Введите второе целое число: ");
+string userInput_2 = Console.ReadLine();
+
+int number_1 = int.Parse(userInput_1);
+int number_2 = int.Parse(userInput_2);
+
+if (number_1 == number_2 * number_2)
+{
+    Console.WriteLine ("Yes");
+}
+else
+{
+    Console.WriteLine ("No");
+}
