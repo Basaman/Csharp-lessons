3c9f0d92009df6099b7ce07b16a6fdc3c8c2927f
ilya <ilia.arkhangelsky@gmail.com>
2022-12-16 11:50:24 +0300

    days week numbers

---
+++ Example010_days_of_week/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write("Введите целое число от 1 до 7: ");
+
+int number = int.Parse(Console.ReadLine() ?? "");
+
+if (number == 1)
+{
+    Console.WriteLine ("Monday");
+}
+if (number == 2)
+{
+    Console.WriteLine ("Tuesday");
+}
+if (number == 3)
+{
+    Console.WriteLine ("Wednesday");
+}
+if (number == 4)
+{
+    Console.WriteLine ("Thursday");
+}
+if (number == 5)
+{
+    Console.WriteLine ("Friday");
+}
+if (number == 6)
+{
+    Console.WriteLine ("Saturday");
+}
+if (number == 7)
+{
+    Console.WriteLine ("Sunday");
+}
