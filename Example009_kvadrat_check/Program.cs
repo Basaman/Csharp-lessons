f81a581ce3e123abde00986ec438520d59b262dd
ilya <ilia.arkhangelsky@gmail.com>
2022-12-16 11:24:11 +0300

    исправлена ошибка

--- Example009_kvadrat_check/Program.cs
+++ Example009_kvadrat_check/Program.cs
@@ -1,16 +1,16 @@
 ﻿Console.Write("Введите целое число: ");
-string userInput_1 = Console.ReadLine();
+string userInput_1 = Console.ReadLine() ?? "";
 Console.Write("Введите второе целое число: ");
-string userInput_2 = Console.ReadLine();
+string userInput_2 = Console.ReadLine() ?? "";
 
 int number_1 = int.Parse(userInput_1);
 int number_2 = int.Parse(userInput_2);
 
 if (number_1 == number_2 * number_2)
 {
-    Console.WriteLine ("Yes");
+    Console.WriteLine ("Да, является");
 }
 else
 {
-    Console.WriteLine ("No");
+    Console.WriteLine ("Нет, не является");
 }
