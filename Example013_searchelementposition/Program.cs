ece4b1c34543b8b4fa1ee56abc1acc5eccba7c7e
ilya <ilia.arkhangelsky@gmail.com>
2022-12-19 16:18:10 +0300

    ititial commit

---
+++ Example013_searchelementposition/Program.cs
@@ -0,0 +1,15 @@
+﻿int [] array = { 111, 222, 34, 46, 555, 62, 77, 222, 81};
+
+int n = array.Length;
+int find = 81;
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
