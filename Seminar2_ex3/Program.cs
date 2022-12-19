19fc473160d0e70ae0cd7e6fb9a794ad6337383c
ilya <ilia.arkhangelsky@gmail.com>
2022-12-19 21:21:27 +0300

    Семинар Задача3

---
+++ Seminar2_ex3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine ("Введите число 1: ");
+string userInput1 = Console.ReadLine() ?? "";
+Console.WriteLine ("Введите число 2: ");
+string userInput2 = Console.ReadLine() ?? "";
+
+int a1 = int.Parse(userInput1);
+int a2 = int.Parse(userInput2);
+
+if (a1 % a2 == 0)
+{
+    Console.WriteLine("кратно");
+}
+else
+{
+    int result = a1 % a2;
+    Console.WriteLine(result);
+}
