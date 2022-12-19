9d2cd7ef02ab07b11b36fd064578202bbad2a0cd
ilya <ilia.arkhangelsky@gmail.com>
2022-12-19 21:22:19 +0300

    Семинар задача2

---
+++ Seminar2_ex2/Program.cs
@@ -0,0 +1,11 @@
+﻿var rnd = new Random();
+int num = rnd.Next(100, 1000);
+
+int a1 = num / 100;
+
+int a2 = num % 100;
+int a3 = a2 % 10;
+
+int result = a1 * 10 + a3;
+
+Console.WriteLine($"{num} -> {result}");
