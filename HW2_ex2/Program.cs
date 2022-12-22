9ca21108e893dcc4e767f80b28671e37dd634a13
ilya <ilia.arkhangelsky@gmail.com>
2022-12-22 17:30:46 +0300

    промежуточная версия, не работает с числом 100

---
+++ HW2_ex2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число: ");
+string userInput = Console.ReadLine() ?? "";
+
+int num = int.Parse(userInput);
+
+if (num < 100 && num > -100) Console.WriteLine("Число не содержит третью цифру");
+if (num > 999 || num < -99)
+{
+    while (num > 999)
+    {
+        num = num / 10;
+    }
+    num = num % 10;
+    Console.WriteLine(num);
+}
+
+
+
