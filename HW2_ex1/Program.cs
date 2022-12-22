61338178ec30d31a054282cfed26efd37d1df7dd
ilya <ilia.arkhangelsky@gmail.com>
2022-12-22 15:13:35 +0300

    программа, которая принимает 3-х значное число, выдает его вторую цифру

---
+++ HW2_ex1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите трёхзначное число: ");
+string userInput = Console.ReadLine() ?? "";
+
+int num = int.Parse(userInput);
+
+if (num < 100 || num > 999)
+{
+    Console.WriteLine("Число не является трёхзначным, попробуйте еще раз");
+}
+else
+{
+    int num1 = num % 100;
+    int num2 = num1 % 10;
+    int num3 = (num1 - num2)/10;
+    Console.WriteLine ($"{num} -> {num3}");
+}
